c7574ac42dd561263b0b86b74366ae076f05a2ff
xventrux <vvyacheslavx@gmail.com>
2022-02-14 21:53:20 +0500

    Добавил контроллер, для регистрации, авторизации и работы с личным кабинетом

---
+++ Site/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
