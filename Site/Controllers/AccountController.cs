5d9f7b2a1f0d84e11a063ef3c7070e8b9c858eb8
xventrux <vvyacheslavx@gmail.com>
2022-02-14 23:01:29 +0500

    Реализовал post метод для регистрации

--- Site/Controllers/AccountController.cs
+++ Site/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Site.Models.Entities;
 using Site.ViewModels.Account;
 using System;
 using System.Collections.Generic;
@@ -9,6 +11,16 @@ namespace Site.Controllers
 {
     public class AccountController : Controller
     {
+
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         [HttpGet]
         public IActionResult Login(string returnUrl = null)
         {
@@ -19,6 +31,38 @@ namespace Site.Controllers
         public IActionResult Registration(string returnUrl = null)
         {
             return View(new RegistrationViewModel() { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Registration(RegistrationViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User { Email = model.Email };
+
+                // добавляем пользователя
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    // установка куки
+                    await _signInManager.SignInAsync(user, false);
+
+                    if(!String.IsNullOrEmpty(model.ReturnUrl))
+                    {
+                        return RedirectToRoute(model.ReturnUrl);
+                    }
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
         }
     }
 }
