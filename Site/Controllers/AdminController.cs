d19e1b3621dbfa0c31b11c500edff2b6ebf36b5d
xventrux <vvyacheslavx@gmail.com>
2022-02-17 17:18:27 +0500

    Реализовал добавление и вывод ролей через ajax

--- Site/Controllers/AdminController.cs
+++ Site/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,38 @@ namespace Site.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRoleAsync(string name)
+        {
+            await _roleManager.CreateAsync(new IdentityRole(name.ToLower()));
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRoles()
+        {
+            List<string> roles = new List<string>();
+
+            var list = _roleManager.Roles;
+
+            foreach(var item in list)
+            {
+                roles.Add(item.Name);
+            }
+
+            return new JsonResult(roles);
         }
     }
 }
