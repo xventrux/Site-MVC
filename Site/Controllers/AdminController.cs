027bc47e92cb2b7ca24176506def669be432b864
xventrux <vvyacheslavx@gmail.com>
2022-02-18 21:24:20 +0500

    Реализовал поиск пользователей по почте

--- Site/Controllers/AdminController.cs
+++ Site/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Site.Models.Entities;
+using Site.ViewModels.Admin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -60,11 +61,63 @@ namespace Site.Controllers
         #endregion
 
         [HttpGet]
-        public async Task<IActionResult> GetUsers()
+        public async Task<IActionResult> GetUsers(string search, string[] _roles)
         {
-            List<User> list = _userManager.Users.ToList();
 
-            return new JsonResult(list);
+            List<User> users = SearchUsers(_userManager.Users, search);
+
+            List<string> roles = new List<string>();
+
+            var list = _roleManager.Roles;
+
+            foreach (var item in list)
+            {
+                roles.Add(item.Name);
+            }
+
+
+            return new JsonResult(new UsersTableViewModel { ListOfUsers = users, ListOfRoles = roles });
+        }
+
+        public List<User> SearchUsers(IQueryable<User> users, string str, string[] roles = null)
+        {
+            List<User> list = new List<User>();
+
+            if (String.IsNullOrEmpty(str))
+            {
+                list = users.ToList();
+            }
+
+            else
+            {
+                list = users.Where(u => u.Email.ToLower().Contains(str.ToLower()) || str.ToLower().Contains(u.Email.ToLower())).ToList();
+            }
+
+            return list;
+        }
+
+        public async Task<IActionResult> ChangeRole(string id, string name)
+        {
+            User user = _userManager.Users.Where(u => u.Id == id).FirstOrDefault();
+
+            if(user == null)
+            {
+                return new JsonResult("Пользователя не существует");
+            }
+
+            IdentityRole selectedRole = _roleManager.Roles.Where(r => r.Name == name).FirstOrDefault();
+
+            if (selectedRole == null)
+            {
+                return new JsonResult("Роли не существует");
+            }
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+            await _userManager.RemoveFromRolesAsync(user, userRoles);
+
+            await _userManager.AddToRoleAsync(user, selectedRole.Name);
+
+            return new JsonResult($"Роль пользователя {user.Email} успешно изменена на {name}");
         }
     }
 }
